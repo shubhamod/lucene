bb0de95a6f5af6483c9eaaa1d412cd2f3afb0b0d
Jonathan Ellis <jbellis@datastax.com>
2023-05-12 13:16:20 -0500

    logsplit

--- logsplit.py
+++ logsplit.py
@@ -2,31 +2,29 @@ import sys
 import re
 
 # Define the start and end strings
-start_construction_str = r"selective ords"
+start_construction_str = r"\d+ adding node \d+"
 end_construction_str = r"Found results"
+start_graph_str = "# Level 0"
 end_graph_str = "java.lang.AssertionError: "
 
 # Initialize the flags
 in_construction = False
 in_graph = False
-skip_next_line = False
 
 # Open the input and output files
 with open("badgraph-construction.txt", "w") as construction_file, \
         open("badgraph.txt", "w") as graph_file:
     # Read the log file line by line
     for line in sys.stdin:
-        if skip_next_line:
-            skip_next_line = False
-            continue
         # Check if the line matches the start or end strings
-        if start_construction_str in line:
+        if re.match(start_construction_str, line):
             in_construction = True
-            skip_next_line = True
             continue
         elif end_construction_str in line:
             in_construction = False
+        elif line.startswith(start_graph_str):
             in_graph = True
+            continue
         elif line.startswith(end_graph_str):
             in_graph = False
             break
@@ -34,6 +32,5 @@ with open("badgraph-construction.txt", "w") as construction_file, \
         # Write the line to the corresponding output file
         if in_construction:
             construction_file.write(line)
-            skip_start_line = False
         elif in_graph:
             graph_file.write(line)
